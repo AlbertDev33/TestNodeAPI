907396166d3e4709590859ded40913e9c64e98af
AlbertDev33 <alber725@gmail.com>
2021-02-18 16:55:49 -0300

    feat: return a list of products

--- src/app.js
+++ src/app.js
@@ -4,8 +4,14 @@ const app = express();
 
 app.use(express.json());
 
-app.get('/', (request, response) => {
-  response.send('Hello World');
+app.get('/products', (request, response) => {
+  response.send([
+    {
+      name: 'Default product',
+      description: 'product description',
+      price: 100,
+    },
+  ]);
 });
 
 export default app;
